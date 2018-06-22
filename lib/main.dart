153215c9e57a50375d1dd114e739f3e776e692b7
Alexander Smith <asmit147@ycp.edu>
2018-06-22 09:35:00 -0400

    plugin is in, trying to get it to print #6

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
 
 import './view/landing_view.dart';
+import './models/bluetooth_model.dart';
 
 void main() {
   runApp(new MaterialApp(
-    home: new LandingView(),
+    home: new BluetoothSearch(),
   ));
 }
--- lib/models/bluetooth_model.dart
+++ lib/models/bluetooth_model.dart
@@ -2,11 +2,36 @@ import 'package:flutter/material.dart';
 
 import 'package:flutter_blue/flutter_blue.dart';
 
-
 FlutterBlue flutterBlue = FlutterBlue.instance;
 
 /// Start scanning
 var scanSubscription = flutterBlue.scan().listen((scanResult) {
-    // do something with scan result
-    scanResult.toString();
+  // do something with scan result
 });
+
+class BluetoothSearch extends StatefulWidget {
+  const BluetoothSearch({Key key}) : super(key: key);
+
+  @override
+  _BluetoothSearchState createState() => new _BluetoothSearchState();
+}
+
+class _BluetoothSearchState extends State<BluetoothSearch> {
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text("LitBit Companion App"),
+      ),
+      body: new Center(
+          child: new Column(children: <Widget>[
+        new Text(
+          scanSubscription.toString(),
+          textAlign: TextAlign.center,
+          overflow: TextOverflow.ellipsis,
+          style: new TextStyle(fontWeight: FontWeight.bold),
+        ),
+      ])),
+    );
+  }
+}
